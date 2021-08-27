388a4d415a96402b788b5c5e64e37ecc225c24b0
Vinicius <Vinicius@192.168.0.164>
2021-08-27 11:59:44 -0300

    projeto inicial

---
+++ src/main/java/br/com/viniciusrvk/challeng_T/ChallengTApplication.java
@@ -0,0 +1,13 @@
+package br.com.viniciusrvk.challeng_T;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengTApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/viniciusrvk/challeng_T/ChallengTApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.viniciusrvk.challeng_T;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
